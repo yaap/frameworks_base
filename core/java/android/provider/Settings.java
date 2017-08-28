2651bd436ed152183742c452427c054ede05f33a
maxwen <max.weninger@gmail.com>
2017-08-28 03:37:08 +0200

    base: advanced reboot with fastboot
    correct default value of advanced reboot
    
    Change-Id: I030c636b06036847236a3cbb5d848bb2707a3e4a
    
    base: correct strings for advanced reboot
    
    add a customReboot API that is used for advanced
    reboot fromm GlobaActions and use correct strings
    for the reboot progress dialog
    
    [1/2] base: expose rebootCustom for OpenDelta
    
    Dont scare people by showing the wrong reboot progress
    
    Adapted for android 7.0
    
    base: use new shutdown ui for all reboot actions
    Change-Id: I22af2cd9c2656978868ca05a31566d166f56a4f4
    
    base: add reboot fastboot to power buttons menu
    
    Change-Id: Ifa1666eb09aa48979c6bd3e2a613a52e731bd4c4
    
    base: Edit advanced reboot action for Android-12
    
    Change-Id: If80afe88f5dc945bec16c84822d44fa368279083

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5762,6 +5762,13 @@ public final class Settings {
                 "screen_flash_notification_color_global";
 
         /**
+         * Whether to enable advanced reboot
+         * @hide
+         */
+        @Readable
+        public static final String OMNI_ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
