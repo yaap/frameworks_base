cd18e995c187b2a06e4804621729d6234cd84e0f
ezio84 <brabus84@gmail.com>
2018-02-14 00:24:18 +0100

    BatteryMeterView: Show a bolt ⚡when charging
    idoybh:
    * Only when using the text only style
    * Improve some code visually: make it easier to read, use one less var, correctly indent and don't call an inner function var with a name format used for globals
    
    Change-Id: I27f745e3458fda7c415a75d6e52f183e327cb498

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -400,7 +400,13 @@ public class BatteryMeterView extends LinearLayout implements
     }
 
     private void setPercentTextAtCurrentLevel() {
-        mBatteryPercentView.setText(
+        if (mBatteryPercentView == null) return;
+        // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+        // to load its emoji colored variant with the uFE0E flag
+        // only use it when there is no batt icon showing
+        String indication = mCharging && (mBatteryStyle == BATTERY_STYLE_TEXT)
+                ? "\u26A1\uFE0E " : "";
+        mBatteryPercentView.setText(indication +
                 NumberFormat.getPercentInstance().format(mLevel / 100f));
         setContentDescription(
                 getContext().getString(mCharging ? R.string.accessibility_battery_level_charging
