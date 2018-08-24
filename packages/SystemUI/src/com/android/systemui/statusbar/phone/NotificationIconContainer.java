91ddeed192a0f799061c9de588447bc264939bab
LuK1337 <priv.luk@gmail.com>
2018-08-24 11:51:35 +0200

    SystemUI: Allow overlaying max notification icons
    * I believe there are devices in this world that can fit
      more than 4 notification icons.
    
    Change-Id: I8f01b4e0385b35f4ed93aab2619839e5b40ee0c7
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
@@ -137,11 +137,10 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
         }
     }.setDuration(CONTENT_FADE_DURATION);
 
-    private static final int MAX_ICONS_ON_AOD = 3;
-
-    /* Maximum number of icons in short shelf on lockscreen when also showing overflow dot. */
-    public static final int MAX_ICONS_ON_LOCKSCREEN = 3;
-    public static final int MAX_STATIC_ICONS = 4;
+    private final int MAX_ICONS_ON_LOCKSCREEN =
+            getResources().getInteger(R.integer.config_maxVisibleNotificationIconsOnLock);
+    public final int MAX_STATIC_ICONS =
+            getResources().getInteger(R.integer.config_maxVisibleNotificationIcons);
     private static final int MAX_DOTS = 1;
 
     private boolean mIsStaticLayout = true;
@@ -408,7 +407,7 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
         float translationX = getActualPaddingStart();
         int firstOverflowIndex = -1;
         int childCount = getChildCount();
-        int maxVisibleIcons = mOnLockScreen ? MAX_ICONS_ON_AOD :
+        int maxVisibleIcons = mOnLockScreen ? MAX_ICONS_ON_LOCKSCREEN :
                 mIsStaticLayout ? MAX_STATIC_ICONS : childCount;
         float layoutEnd = getLayoutEnd();
         mVisualOverflowStart = 0;
