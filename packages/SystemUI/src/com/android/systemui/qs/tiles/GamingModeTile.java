3bbd1515d4420212d3a53df3b64bf9a680014c91
spezi77 <spezi7713@gmx.net>
2018-10-19 17:57:23 +0200

    SystemUI: GamingModeTile: Show "What's This?" on long click
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Change-Id: Id4ab8c08089dd7783016df75cb6090c23ae9b658

--- packages/SystemUI/src/com/android/systemui/qs/tiles/GamingModeTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/GamingModeTile.java
@@ -95,6 +95,10 @@ public class GamingModeTile extends QSTileImpl<BooleanState> {
             enableGamingMode();
             return;
         }
+        showGamingModeWhatsThisDialog();
+    }
+
+    private void showGamingModeWhatsThisDialog() {
         SystemUIDialog dialog = new SystemUIDialog(mContext);
         dialog.setTitle(R.string.gaming_mode_dialog_title);
         dialog.setMessage(R.string.gaming_mode_dialog_message);
@@ -118,6 +122,11 @@ public class GamingModeTile extends QSTileImpl<BooleanState> {
     @Override
     public Intent getLongClickIntent() {
         return null;
+    }
+
+    @Override
+    protected void handleLongClick(@Nullable View view) {
+        showGamingModeWhatsThisDialog();
     }
 
     private void handleState(boolean enabled) {
