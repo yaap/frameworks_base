d4ae9062d4ddca2a7c4e8f6f723650cff2acad76
SuperDroidBond <superdroidbond@yahoo.com>
2019-11-26 22:46:49 +0530

    NetworkTraffic: Fix Net Text Getting Cut at DPI Changes
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -74,7 +74,6 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     private static final int MB = KB * KB;
     private static final int GB = MB * KB;
     private static final String symbol = "B/s";
-    private int mNetTrafSize = 21;
 
     private static DecimalFormat decimalFormat = new DecimalFormat("##0.#");
     static {
@@ -92,6 +91,7 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     private int txtImgPadding;
     private int mTrafficType;
     private int mAutoHideThreshold;
+    private int mNetTrafSize;
     private int mTintColor;
     private int mVisibleState = -1;
     private boolean mTrafficVisible = false;
@@ -259,6 +259,9 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.NETWORK_TRAFFIC_FONT_SIZE), false,
+                    this, UserHandle.USER_ALL);
         }
 
         /*
@@ -291,6 +294,9 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     public NetworkTrafficSB(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         final Resources resources = getResources();
+        txtSize = (mTrafficType == BOTH)
+                    ? resources.getDimensionPixelSize(R.dimen.net_traffic_multi_text_size)
+                    : mNetTrafSize;
         txtImgPadding = resources.getDimensionPixelSize(R.dimen.net_traffic_txt_img_padding);
         mTintColor = resources.getColor(android.R.color.white);
         Handler mHandler = new Handler();
@@ -387,6 +393,9 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
         mTrafficInHeaderView = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION, 0,
                 UserHandle.USER_CURRENT) == 1;
+        mNetTrafSize = Settings.System.getIntForUser(resolver,
+                Settings.System.NETWORK_TRAFFIC_FONT_SIZE, 21,
+                UserHandle.USER_CURRENT);
     }
 
     private void clearHandlerCallbacks() {
@@ -444,11 +453,6 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     }
 
     private void updateTextSize() {
-        int txtSize;
-
-        mNetTrafSize = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.NETWORK_TRAFFIC_FONT_SIZE, 21);
-
         if (mTrafficType == BOTH) {
             txtSize = getResources().getDimensionPixelSize(R.dimen.net_traffic_multi_text_size);
         } else {
@@ -459,8 +463,12 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
 
     public void onDensityOrFontScaleChanged() {
         final Resources resources = getResources();
+        txtSize = (mTrafficType == BOTH)
+		    ? resources.getDimensionPixelSize(R.dimen.net_traffic_multi_text_size)
+		    : mNetTrafSize;
         txtImgPadding = resources.getDimensionPixelSize(R.dimen.net_traffic_txt_img_padding);
         setCompoundDrawablePadding(txtImgPadding);
+        setTextSize(TypedValue.COMPLEX_UNIT_PX, (float)txtSize);
         setGravity(Gravity.RIGHT);
         updateTextSize();
     }
