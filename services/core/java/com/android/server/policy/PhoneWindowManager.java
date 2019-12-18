4d9528f149bafccf7df484ff4bd636d1e5dd3aa4
spezi77 <spezi7713@gmx.net>
2019-12-18 23:28:36 +0100

    base: Add haptic feedback for torch when screen is off
    Partially based on: https://github.com/ezio84/abc_frameworks_base/commit/bd51d9d4805d18917f8d76ca44a4868dfd5b835c
    
    So thanks very much @ezio84
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Iadd69277258d99eb3e9618d527038a2a86a38448

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -744,6 +744,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     handleRingerChordGesture();
                     break;
                 case MSG_TOGGLE_TORCH:
+                    performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false, "Torch Long-Press");
                     toggleTorch();
                     break;
             }
@@ -1132,7 +1133,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void powerPress(long eventTime, boolean interactive, int count) {
-        if (mDefaultDisplayPolicy.isScreenOnEarly() && !mDefaultDisplayPolicy.isScreenOnFully()) {
+        if (!isDozeMode() && mDefaultDisplayPolicy.isScreenOnEarly() && !mDefaultDisplayPolicy.isScreenOnFully()) {
             Slog.i(TAG, "Suppressed redundant power key press while "
                     + "already in the process of turning the screen on.");
             return;
