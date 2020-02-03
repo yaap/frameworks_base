dbbab53d149d478426333034872b2011592e0303
Zhuying Li <vend_wsd_am_027@mediatek.com>
2020-02-03 19:45:43 +0800

    PMS and WMS deadlock when uninstalling APP
    1. mGlobalLock in WMS is same as mGlobalLockWithoutBoost in AMS.
    2. mPackages in PMS is held when uninstalling email. Need mGlobalLockWithoutBoost
           in AMS when calling AMS API to kill email process.
    3. mGlobalLock" in WMS is held when calling WMS API to add window. Need mPackages
           in PMS when calling PMS API to check whether this package is suspended.
    
    Bug: 148824141
    Change-Id: Ifcfad8151d81d95435edb04081b807920a296abb
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -1465,6 +1465,7 @@ public class WindowManagerService extends IWindowManager.Stub
         final int callingPid = Binder.getCallingPid();
         final long origId = Binder.clearCallingIdentity();
         final int type = attrs.type;
+        final boolean suspended = mPmInternal.isPackageSuspended(attrs.packageName, UserHandle.getUserId(session.mUid));
 
         synchronized (mGlobalLock) {
             if (!mDisplayReady) {
@@ -1760,9 +1761,6 @@ public class WindowManagerService extends IWindowManager.Stub
             win.attach();
             mWindowMap.put(client.asBinder(), win);
             win.initAppOpsState();
-
-            final boolean suspended = mPmInternal.isPackageSuspended(win.getOwningPackage(),
-                    UserHandle.getUserId(win.getOwningUid()));
             win.setHiddenWhileSuspended(suspended);
 
             final boolean hideSystemAlertWindows = !mHidingNonSystemOverlayWindows.isEmpty();
