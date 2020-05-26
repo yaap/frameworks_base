277a759e680af553c8fa9b3ada819894c85372e4
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: Re-evaluate system theme on UI mode change
    - Need for power menu to set accurate colors
    
    Change-Id: I05d41eaf8ea19ce3b6ce659d01da33cf55de3b7e
    
    [YAAP:] adapt for YaapThemeOverlayController

--- packages/SystemUI/src/com/yaap/android/systemui/theme/YaapThemeOverlayController.kt
+++ packages/SystemUI/src/com/yaap/android/systemui/theme/YaapThemeOverlayController.kt
@@ -44,6 +44,8 @@ import com.android.systemui.flags.FeatureFlags
 import com.android.systemui.keyguard.WakefulnessLifecycle
 import com.android.systemui.settings.UserTracker
 import com.android.systemui.statusbar.policy.DeviceProvisionedController
+import com.android.systemui.statusbar.policy.ConfigurationController;
+import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.theme.ThemeOverlayApplier
 import com.android.systemui.theme.ThemeOverlayController
 import com.android.systemui.util.settings.SecureSettings
@@ -81,6 +83,7 @@ class YaapThemeOverlayController @Inject constructor(
     dumpManager: DumpManager,
     featureFlags: FeatureFlags,
     wakefulnessLifecycle: WakefulnessLifecycle,
+    configurationController: ConfigurationController,
 ) : ThemeOverlayController(
     context,
     broadcastDispatcher,
@@ -96,6 +99,7 @@ class YaapThemeOverlayController @Inject constructor(
     dumpManager,
     featureFlags,
     wakefulnessLifecycle,
+    configurationController,
 ) {
     private val settingsObserver: ContentObserver
 
