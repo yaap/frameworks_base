yaap/frameworks_base

commit f1ed9ba810acc614871b44ec4683f77a16f79747

base: SystemUI: add back QS hide label
if enabled label click (secondary) will be reassigned to long click
for dual target tiles

@idoybh edits: Adapt to A11, improve some code while at it

Change-Id: I440362ab38057cbfba7750d75825179bd830c4e8

Co-authored-by: idoybh <idoybh2@gmail.com>
Change-Id: I54f3aa80e940a22f187e2b57083113c5090050c3

packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -31,6 +31,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -208,9 +209,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         if (mRegularTileLayout instanceof PagedTileLayout) {
             mQsTileRevealController = new QSTileRevealController(mContext, this,
                     (PagedTileLayout) mRegularTileLayout);
-            updateSettings();
         }
         mQSLogger.logAllTilesChangeListening(mListening, getDumpableTag(), mCachedSpecs);
+        updateSettings();
         updateResources();
     }
 
@@ -883,6 +884,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
         if (mTileLayout != null) {
             mTileLayout.addTile(r);
+            configureTile(r.tile, r.tileView);
         }
 
         return r;
@@ -1188,6 +1190,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         boolean updateResources();
         int getNumColumns();
         void updateSettings();
+        boolean isShowTitles();
 
         void setListening(boolean listening);
 
@@ -1218,9 +1221,32 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         int getNumVisibleTiles();
     }
 
+    private void configureTile(QSTile t, QSTileView v) {
+        if (mTileLayout != null) {
+            v.setHideLabel(!mTileLayout.isShowTitles());
+            if (t.isDualTarget()) {
+                if (!mTileLayout.isShowTitles()) {
+                    v.setOnLongClickListener(view -> {
+                        t.secondaryClick();
+                        return true;
+                    });
+                } else {
+                    v.setOnLongClickListener(view -> {
+                        t.longClick();
+                        return true;
+                    });
+                }
+            }
+        }
+    }
+
     public void updateSettings() {
         if (mTileLayout != null) {
             mTileLayout.updateSettings();
+
+            for (TileRecord r : mRecords) {
+                configureTile(r.tile, r.tileView);
+            }
         }
     }
 

packages/SystemUI/plugin/src/com/android/systemui/plugins/qs/QSTile.java
@@ -69,6 +69,7 @@ public interface QSTile {
     CharSequence getTileLabel();
 
     State getState();
+    boolean isDualTarget();
 
     default LogMaker populate(LogMaker logMaker) {
         return logMaker;

packages/SystemUI/plugin/src/com/android/systemui/plugins/qs/QSTileView.java
@@ -50,4 +50,8 @@ public abstract class QSTileView extends LinearLayout {
     public abstract void onStateChanged(State state);
 
     public abstract int getDetailY();
+
+    public void setHideLabel(boolean value) {
+        // empty by default
+    }
 }

packages/SystemUI/res/menu/qs_customize_menu.xml
@@ -41,6 +41,9 @@ limitations under the License.
         <item android:id="@+id/menu_item_columns_landscape"
               android:title="@string/qs_menu_item_columns_landscape">
             <menu>
+                <item android:id="@+id/menu_item_columns_landscape_three"
+                      android:title="@string/qs_menu_item_columns_three"
+                      android:checkable="true" />
                 <item android:id="@+id/menu_item_columns_landscape_four"
                       android:title="@string/qs_menu_item_columns_four"
                       android:checkable="true" />
@@ -75,6 +78,9 @@ limitations under the License.
                       android:checkable="true" />
             </menu>
         </item>
+        <item android:id="@+id/menu_item_titles"
+              android:title="@string/qs_menu_item_titles"
+              android:checkable="true" />
         <item android:id="@+id/menu_item_reset"
               android:title="@string/qs_menu_item_reset"/>
 </menu>

packages/SystemUI/res/values/yaap_dimens.xml
@@ -1,24 +1,18 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!--
-     Copyright (C) 2016 The Android Open Source Project
-
+     Copyright (C) 2019 The PixelDust Project
      Licensed under the Apache License, Version 2.0 (the "License");
      you may not use this file except in compliance with the License.
      You may obtain a copy of the License at
-
           http://www.apache.org/licenses/LICENSE-2.0
-
      Unless required by applicable law or agreed to in writing, software
      distributed under the License is distributed on an "AS IS" BASIS,
      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
      See the License for the specific language governing permissions and
      limitations under the License.
 -->
+<resources>
 
-<FrameLayout
-    xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_height="wrap_content"
-    android:layout_width="match_parent"
-    android:layout_marginTop="@dimen/qs_tile_margin_top_bottom"
-    android:layout_marginBottom="@dimen/qs_tile_margin_top_bottom"
-    android:gravity="center" />
+    <dimen name="qs_tile_height_wo_label">48dp</dimen>
+
+</resources>

packages/SystemUI/res/values/yaap_strings.xml
@@ -94,5 +94,6 @@
     <string name="qs_menu_item_reset">Reset</string>
     <string name="qs_menu_item_qs_columns">Quickbar columns</string>
     <string name="qs_menu_item_columns_auto">Automatic</string>
+    <string name="qs_menu_item_titles">Show labels</string>
 
 </resources>

packages/SystemUI/src/com/android/systemui/qs/DoubleLineTileLayout.kt
@@ -123,6 +123,14 @@ class DoubleLineTileLayout(
         setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), height)
     }
 
+    override fun isShowTitles(): Boolean {
+        return false
+    }
+
+    override fun getNumColumns(): Int {
+        return 6
+    }
+
     private fun calculateMaxColumns(availableWidth: Int): Int {
         if (smallTileSize + cellMarginHorizontal == 0) {
             return 0

packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
@@ -587,4 +587,14 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
         }
         distributeTiles();
     }
+
+    @Override
+    public int getNumColumns() {
+        return mPages.get(0).getNumColumns();
+    }
+
+    @Override
+    public boolean isShowTitles() {
+        return mPages.get(0).isShowTitles();
+    }
 }

commit 226af6f8368a49720e31958f856ff7b214333bf7

SystemUI: QSPanel: Do not reassign long click listener
Breaks long click for quick qs header
introduced in: f1ed9ba810ac

Change-Id: I788882041ee771ceffdb53bb15c6991420cd19b5

packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -1340,19 +1340,6 @@ public class QSPanel extends LinearLayout implements Callback, BrightnessMirrorL
     private void configureTile(QSTile t, QSTileView v) {
         if (mTileLayout != null) {
             v.setHideLabel(!mTileLayout.isShowTitles());
-            if (t.isDualTarget()) {
-                if (!mTileLayout.isShowTitles()) {
-                    v.setOnLongClickListener(view -> {
-                        t.secondaryClick();
-                        return true;
-                    });
-                } else {
-                    v.setOnLongClickListener(view -> {
-                        t.longClick();
-                        return true;
-                    });
-                }
-            }
         }
     }
 