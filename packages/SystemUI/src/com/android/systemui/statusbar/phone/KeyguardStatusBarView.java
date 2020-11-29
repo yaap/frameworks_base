3043a2b973f4345f21b6d6dd71349039d247601e
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-29 02:08:20 +0200

    base: Allow choosing whether to show % inside battery icon [1/2]
    Change-Id: I5987cf9fee6ea59532e8d7f2f484be9b1dfa9ee3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -206,7 +206,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 mMultiUserAvatar.setVisibility(View.GONE);
             }
         }
-        mBatteryView.setForceShowPercent(mBatteryCharging);
     }
 
     private void updateSystemIconsLayoutParams() {
