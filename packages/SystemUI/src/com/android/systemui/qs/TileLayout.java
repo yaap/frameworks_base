bc41d1d1c6c41156cce6d07f486ece2a30fb9c56
Ido Ben-Hur <idoybh2@gmail.com>
2021-01-06 16:52:33 +0200

    SystemUI: Have a constant default value for QS columns
    Change-Id: Ib35057049d17231d2dad0ad241202080a94e5748

--- packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
@@ -250,14 +250,13 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
 
     public void updateSettings() {
         final Resources res = mContext.getResources();
-        int defaultColumns = Math.max(1, res.getInteger(R.integer.quick_settings_num_columns));
         boolean isPortrait = res.getConfiguration().orientation
                 == Configuration.ORIENTATION_PORTRAIT;
         int columns = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS, defaultColumns,
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS, 3,
                 UserHandle.USER_CURRENT);
         int columnsLandscape = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, defaultColumns,
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, 4,
                 UserHandle.USER_CURRENT);
         boolean showTitles = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
--- packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -494,12 +494,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         final Resources res = mContext.getResources();
         boolean isPortrait = res.getConfiguration().orientation
                 == Configuration.ORIENTATION_PORTRAIT;
-        int defaultColumns = Math.max(1, mContext.getResources().getInteger(R.integer.quick_settings_num_columns));
         int columns = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS, defaultColumns,
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS, 3,
                 UserHandle.USER_CURRENT);
         int columnsLandscape = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, defaultColumns,
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, 4,
                 UserHandle.USER_CURRENT);
         int rows = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS, 3,
@@ -510,11 +509,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         mTileAdapter.setColumnCount(isPortrait ? columns : columnsLandscape);
         mTileAdapter.setHideLabel(!showTitles);
         mLayout.setSpanCount(isPortrait ? columns : columnsLandscape);
-        updateColumnsMenu(defaultColumns, columns, columnsLandscape);
+        updateColumnsMenu(columns, columnsLandscape);
         updateRowsMenu(rows);
     }
 
-    private void updateColumnsMenu(int defaultColumns, int columns, int columnsLandscape) {
+    private void updateColumnsMenu(int columns, int columnsLandscape) {
         MenuItem menuItemThree = mToolbar.getMenu().findItem(R.id.menu_item_columns_three);
         menuItemThree.setChecked(columns == 3);
         MenuItem menuItemFour = mToolbar.getMenu().findItem(R.id.menu_item_columns_four);
