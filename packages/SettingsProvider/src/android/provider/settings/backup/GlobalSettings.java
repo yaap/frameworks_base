e9152dc0067fcdf7198946f85e31c30f93edf7ad
Ido Ben-Hur <idoybh2@gmail.com>
2021-02-17 12:58:19 +0200

    base: Backup mobile data always active setting
    Change-Id: I6e7738ef405ff88a5690e3e03030564dd64ba7c1

--- packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -72,6 +72,7 @@ public class GlobalSettings {
         Settings.Global.NOTIFICATION_BUBBLES,
         Settings.Global.CUSTOM_BUGREPORT_HANDLER_APP,
         Settings.Global.CUSTOM_BUGREPORT_HANDLER_USER,
-        Settings.Global.DEVELOPMENT_SETTINGS_ENABLED
+        Settings.Global.DEVELOPMENT_SETTINGS_ENABLED,
+        Settings.Global.MOBILE_DATA_ALWAYS_ON,
     };
 }
