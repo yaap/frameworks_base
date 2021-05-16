c00cd4ba81a37a04cc960788bb5fd4e24914b416
Ido Ben-Hur <idoybh2@gmail.com>
2021-05-16 04:56:27 +0300

    VolumeDialogImpl: Optimize the expand animation
    We don't need to invalidate the whole view for every animation step
    Also 80ms is too fast, using show & hide animation durations
    Some other minor improvements to code & runtime
    
    Change-Id: I4c954bf79313f223cbe22fbd6132b71984a5b27a

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -137,7 +137,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private static final String TAG = Util.logTag(VolumeDialogImpl.class);
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
-    private static final int UPDATE_ANIMATION_DURATION = 80;
+    private static final int UPDATE_ANIMATION_DURATION = 100;
 
     static final int DIALOG_SAFETYWARNING_TIMEOUT_MILLIS = 5000;
     static final int DIALOG_ODI_CAPTIONS_TOOLTIP_TIMEOUT_MILLIS = 5000;
@@ -627,25 +627,22 @@ public class VolumeDialogImpl implements VolumeDialog,
         }
 
         ValueAnimator animator = ValueAnimator.ofInt(startWidth, targetWidth);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                mDialogRowsView.getLayoutParams().width =
-                        (Integer) valueAnimator.getAnimatedValue();
-                mDialogRowsView.requestLayout();
-            }
+        animator.addUpdateListener(valueAnimator -> {
+            ViewGroup.LayoutParams layoutParams = mDialogRowsView.getLayoutParams();
+            layoutParams.width = (Integer) valueAnimator.getAnimatedValue();
+            mDialogRowsView.setLayoutParams(layoutParams);
         });
-        animator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                if (!expand) {
-                    updateExpandedRows(expand);
+        if (!expand) {
+            animator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    updateExpandedRows(false);
                 }
-            }
-        });
+            });
+        }
         animator.setInterpolator(expand ? new SystemUIInterpolators.LogDecelerateInterpolator()
                 : new SystemUIInterpolators.LogAccelerateInterpolator());
-        animator.setDuration(UPDATE_ANIMATION_DURATION);
+        animator.setDuration(expand ? DIALOG_SHOW_ANIMATION_DURATION : DIALOG_HIDE_ANIMATION_DURATION);
         animator.start();
     }
 
