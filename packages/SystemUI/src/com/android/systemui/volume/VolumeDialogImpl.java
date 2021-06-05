90de388f1fefc63e3d8959c2e0ab53a395f20fa5
Ido Ben-Hur <idoybh2@gmail.com>
2021-06-05 11:27:30 +0300

    VolumeDialogImpl: Add a null check for captions icon
    On a very rare edge case this function can be called while
    mODICaptionsIcon is null. Avoid an NPE
    
    Change-Id: I01974985bf020af9a187e73bfec87f347baf69e1

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -818,6 +818,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     }
 
     private void updateCaptionsIcon() {
+        if (mODICaptionsIcon == null) return;
         boolean captionsEnabled = mController.areCaptionsEnabled();
         if (mODICaptionsIcon.getCaptionsEnabled() != captionsEnabled) {
             mHandler.post(mODICaptionsIcon.setCaptionsEnabled(captionsEnabled));
