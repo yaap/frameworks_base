b7a893efb4558b2fd85079f26c97dc5146fa570a
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-06-19 21:20:27 +0530

    SystemUI: FOD: Add function to get FOD height from the bottom of the screen
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>
    Change-Id: I29f295bab71e9477c842c6234e4cc8c16baa44cf
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- packages/SystemUI/src/com/android/systemui/util/Utils.java
+++ packages/SystemUI/src/com/android/systemui/util/Utils.java
@@ -20,17 +20,23 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.RemoteException;
 import android.provider.Settings;
+import android.util.DisplayMetrics;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.android.systemui.shared.system.QuickStepContract;
 import com.android.systemui.statusbar.CommandQueue;
+
+import vendor.lineage.biometrics.fingerprint.inscreen.V1_0.IFingerprintInscreen;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.NoSuchElementException;
 
 public class Utils {
 
@@ -161,5 +167,32 @@ public class Utils {
             apps.add(s);
         }
         return apps;
+    }
+
+    public static int getFODHeight(Context context, boolean includeDecor) {
+        IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
+        if (daemon == null) {
+            return 0;
+        }
+        DisplayMetrics dm = new DisplayMetrics();
+        if (includeDecor) {
+            context.getSystemService(WindowManager.class).getDefaultDisplay().getMetrics(dm);
+        } else {
+            context.getSystemService(WindowManager.class).getDefaultDisplay().getRealMetrics(dm);
+        }
+        try {
+            return dm.heightPixels - daemon.getPositionY() + daemon.getSize() / 2;
+        } catch (NoSuchElementException | RemoteException e) {
+            return 0;
+        }
+    }
+
+    private static IFingerprintInscreen getFingerprintInScreenDaemon() {
+        try {
+            return IFingerprintInscreen.getService();
+        } catch (NoSuchElementException | RemoteException e) {
+            // do nothing
+        }
+        return null;
     }
 }
