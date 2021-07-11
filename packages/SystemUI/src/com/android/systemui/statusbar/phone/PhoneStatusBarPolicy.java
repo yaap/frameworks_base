288fbd540f7b871052739e4bef2a11b1a2be8acb
Ido Ben-Hur <idoybh2@gmail.com>
2021-07-11 14:40:52 +0300

    PhoneStatusBarPolicy: Show battery level for all devices
    There is absolutely no need to restrict it to specific devices
    If the device doesn't support % it will return BATTERY_LEVEL_UNKNOWN anyway
    Change-Id: I43627ff6da689b7b4fa49de047b4ada40d2a542a
    
    Change-Id: I6745adbe89dd5ad38e856966ef3d61c067dfc55b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -454,7 +454,7 @@ public class PhoneStatusBarPolicy
                         if (state == BluetoothProfile.STATE_CONNECTED) {
                             int batteryLevel = device.getBatteryLevel();
                             BluetoothClass type = device.getBtClass();
-                            if (batteryLevel != BluetoothDevice.BATTERY_LEVEL_UNKNOWN && showBatteryForThis(type)) {
+                            if (batteryLevel > BluetoothDevice.BATTERY_LEVEL_UNKNOWN) {
                                 iconId = getBtLevelIconRes(batteryLevel);
                             } else {
                                 iconId = R.drawable.stat_sys_data_bluetooth_connected;
@@ -496,27 +496,6 @@ public class PhoneStatusBarPolicy
         } else {
             return R.drawable.stat_sys_data_bluetooth_connected;
         }
-     }
-
-    private boolean showBatteryForThis(BluetoothClass type) {
-        boolean show = false;
-        if (type != null) {
-            switch (type.getDeviceClass()) {
-            case BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET:
-            case BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE:
-            case BluetoothClass.Device.AUDIO_VIDEO_PORTABLE_AUDIO:
-            case BluetoothClass.Device.AUDIO_VIDEO_LOUDSPEAKER:
-            case BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES:
-                show = true;
-                break;
-            default:
-                show = false;
-                break;
-            }
-        } else {
-            show = false;
-        }
-        return show;
     }
 
     private final void updateTTY() {
