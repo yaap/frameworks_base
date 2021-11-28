596725224804c55f4e6c20dca189d35ef9a88607
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-29 01:51:27 +0530

    Revert "Always show lockdown button"
    * how about no
    
    This reverts commit eb151ee8d0ffaca0acbc68e3ac2548efaa3acc32.

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -125,6 +125,7 @@ public class SecureSettings {
         Settings.Secure.SCREENSAVER_COMPONENTS,
         Settings.Secure.SCREENSAVER_ACTIVATE_ON_DOCK,
         Settings.Secure.SCREENSAVER_ACTIVATE_ON_SLEEP,
+        Settings.Secure.LOCKDOWN_IN_POWER_MENU,
         Settings.Secure.SHOW_FIRST_CRASH_DIALOG_DEV_OPTION,
         Settings.Secure.VOLUME_HUSH_GESTURE,
         Settings.Secure.MANUAL_RINGER_TOGGLE_COUNT,
