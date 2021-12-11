639bbc72218746a215774656b8727e919648cac5
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-11 18:40:18 +0200

    QSPanel: Don't crash when not using media player
    Happens when settings Settings.Global.SHOW_MEDIA_ON_QUICK_SETTINGS to 0
    
    1st log:
    E AndroidRuntime: FATAL EXCEPTION: main
    E AndroidRuntime: Process: com.android.systemui, PID: 22984
    E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.widget.LinearLayout.setVisibility(int)' on a null object reference
    E AndroidRuntime:        at com.android.systemui.qs.QSPanel.setUsingHorizontalLayout(QSPanel.java:733)
    E AndroidRuntime:        at com.android.systemui.qs.QSPanelControllerBase.switchTileLayout(QSPanelControllerBase.java:314)
    E AndroidRuntime:        at com.android.systemui.qs.QSPanelControllerBase.onViewAttached(QSPanelControllerBase.java:159)
    ...
    
    2nd log:
    E AndroidRuntime: Process: com.android.systemui, PID: 13671
    E AndroidRuntime: java.lang.IndexOutOfBoundsException: index=3 count=2
    E AndroidRuntime:        at android.view.ViewGroup.addInArray(ViewGroup.java:5375)
    E AndroidRuntime:        at android.view.ViewGroup.addViewInner(ViewGroup.java:5268)
    E AndroidRuntime:        at android.view.ViewGroup.addView(ViewGroup.java:5076)
    E AndroidRuntime:        at android.view.ViewGroup.addView(ViewGroup.java:5016)
    E AndroidRuntime:        at com.android.systemui.qs.QSPanel.switchToParent(QSPanel.java:841)
    E AndroidRuntime:        at com.android.systemui.qs.QSPanel.switchToParent(QSPanel.java:446)
    E AndroidRuntime:        at com.android.systemui.qs.QSPanel.switchAllContentToParent(QSPanel.java:415)
    E AndroidRuntime:        at com.android.systemui.qs.QSPanel.setUsingHorizontalLayout(QSPanel.java:727)
    E AndroidRuntime:        at com.android.systemui.qs.QSPanelControllerBase.switchTileLayout(QSPanelControllerBase.java:314)
    E AndroidRuntime:        at com.android.systemui.qs.QSPanelControllerBase.onViewAttached(QSPanelControllerBase.java:159)
    ...

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -535,7 +535,9 @@ public class QSPanel extends LinearLayout {
                     Settings.Secure.QS_BRIGHTNESS_POSITION_BOTTOM, 0) == 1;
             if (!mUsingHorizontalLayout) {
                 switchToParent(mBrightnessView, parent, bottom ? index : 0);
-                index++;
+                boolean mediaInQS = Settings.Global.getInt(mContext.getContentResolver(),
+                        Settings.Global.SHOW_MEDIA_ON_QUICK_SETTINGS, 1) == 1;
+                if (mediaInQS) index++;
             } else {
                 setBrightnessPosition(bottom);
             }
@@ -718,6 +720,7 @@ public class QSPanel extends LinearLayout {
                 mTileLayout.setMaxColumns(horizontal ? 2 : 4);
             }
             updateMargins(mediaHostView);
+            if (mHorizontalLinearLayout == null) return;
             mHorizontalLinearLayout.setVisibility(horizontal ? View.VISIBLE : View.GONE);
         }
     }
