f66e893c63c469a08143ebbe2520c6ed55b73f1b
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-23 14:57:19 +0200

    Revert "SystemUI: arrow keys while typing resurrection [1/2]"
    This reverts commit d987999ff3692ab50de4606333a428c5e0dfe26a.

--- packages/SystemUI/src/com/android/systemui/navigationbar/buttons/RotationContextButton.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/buttons/RotationContextButton.java
@@ -29,7 +29,6 @@ public class RotationContextButton extends ContextualButton implements RotationB
     public static final boolean DEBUG_ROTATION = false;
 
     private RotationButtonController mRotationButtonController;
-    private boolean mCanShow = true;
 
     /**
      * @param lightContext the context to use to load the icon resource
@@ -76,21 +75,5 @@ public class RotationContextButton extends ContextualButton implements RotationB
     public boolean acceptRotationProposal() {
         View currentView = getCurrentView();
         return currentView != null && currentView.isAttachedToWindow();
-    }
-
-    @Override
-    public boolean show() {
-        if (!mCanShow) {
-            return false;
-        }
-        return super.show();
-    }
-
-    @Override
-    public void setCanShowRotationButton(boolean canShow) {
-        mCanShow = canShow;
-        if (!mCanShow) {
-            hide();
-        }
     }
 }
