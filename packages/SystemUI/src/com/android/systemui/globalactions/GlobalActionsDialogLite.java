34bf68d6f77f1a41691613254c6ce9b64db274ef
Ido Ben-Hur <idoybh2@gmail.com>
2022-01-06 21:25:50 +0200

    GlobalActionsDialogLite: Allow advanced reboot on (in)secure
    No no dear omni. You reversed that logic. Also no sense in not allowing it.
    Also no sense in checking that here, should have not shown the UI to begin with.

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -733,13 +733,6 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         return advancedRebootEnabled;
     }
 
-    private boolean isSecureLocked() {
-        if (mKeyguardShowing) {
-            return mKeyguardStateController.isUnlocked();
-        }
-        return false;
-    }
-
     @VisibleForTesting
     boolean shouldDisplayLockdown(UserInfo user) {
         if (user == null) {
@@ -985,7 +978,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         @Override
         public void onPress() {
             mUiEventLogger.log(GlobalActionsEvent.GA_REBOOT_PRESS);
-            if (!mRebootMenu && advancedRebootEnabled(mContext) && !isSecureLocked()) {
+            if (!mRebootMenu && advancedRebootEnabled(mContext)) {
                 mRebootMenu = true;
                 mCurrentMenuActions = mRebootMenuActions;
                 createActionItems();
