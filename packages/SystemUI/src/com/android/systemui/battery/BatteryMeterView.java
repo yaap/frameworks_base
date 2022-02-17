97fa8f81dc6a4ec29a093a79081c3d8b521970da
Ido Ben-Hur <idoybh2@gmail.com>
2022-02-17 05:03:48 +0200

    base: SystemUI: Fix circle battery QS tinting
    Battery background arc was tinted too close to differ
    Replace textColorSecodary with textColorHint

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -49,7 +49,6 @@ import android.widget.TextView;
 import androidx.annotation.StyleRes;
 import androidx.annotation.VisibleForTesting;
 
-
 import com.android.settingslib.graph.CircleBatteryDrawable;
 import com.android.settingslib.graph.ThemedBatteryDrawable;
 import com.android.systemui.DualToneHandler;
@@ -73,9 +72,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
     public static final int MODE_ON = 1;
     public static final int MODE_OFF = 2;
     public static final int MODE_ESTIMATE = 3;
-
-    static final String STATUS_BAR_BATTERY_STYLE =
-            Settings.System.STATUS_BAR_BATTERY_STYLE;
 
     public static final int BATTERY_STYLE_PORTRAIT = 0;
     public static final int BATTERY_STYLE_CIRCLE = 1;
@@ -460,6 +456,10 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         if (mUnknownStateDrawable != null) {
             mUnknownStateDrawable.setTint(singleToneColor);
         }
+    }
+
+    public int getBatteryStyle() {
+        return mBatteryStyle;
     }
 
     public void dump(PrintWriter pw, String[] args) {
