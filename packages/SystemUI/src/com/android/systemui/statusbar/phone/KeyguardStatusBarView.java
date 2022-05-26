f3e154e814212e573edd77aceae38a90e9bbc08c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-26 18:39:27 +0530

    SystemUI: Re-inflate keyguard statusbar on theme change
    Fixes lockscreen statusbar icons not refreshing on changing icon pack.
    Inspired by https://github.com/AOSPA/android_frameworks_base/commit/20dc3a97ffbce00f26ff2d96f027eefb826877ee
    
    Change-Id: I1d004c5a54fad5231dfe04e1d134a28293993d6d
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/7832
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -238,11 +238,15 @@ public class KeyguardStatusBarView extends RelativeLayout {
     WindowInsets updateWindowInsets(
             WindowInsets insets,
             StatusBarContentInsetsProvider insetsProvider) {
+        updateWindowInsets(insetsProvider);
+        return super.onApplyWindowInsets(insets);
+    }
+
+    private void updateWindowInsets(StatusBarContentInsetsProvider insetsProvider) {
         mLayoutState = LAYOUT_NONE;
         if (updateLayoutConsideringCutout(insetsProvider)) {
             requestLayout();
         }
-        return super.onApplyWindowInsets(insets);
     }
 
     private boolean updateLayoutConsideringCutout(StatusBarContentInsetsProvider insetsProvider) {
@@ -408,9 +412,11 @@ public class KeyguardStatusBarView extends RelativeLayout {
     }
 
     /** Should only be called from {@link KeyguardStatusBarViewController}. */
-    void onThemeChanged(StatusBarIconController.TintedIconManager iconManager) {
+    void onThemeChanged(StatusBarIconController.TintedIconManager iconManager,
+            StatusBarContentInsetsProvider insetsProvider) {
         mBatteryView.setColorsFromContext(mContext);
         updateIconsAndTextColors(iconManager);
+        updateWindowInsets(insetsProvider);
     }
 
     /** Should only be called from {@link KeyguardStatusBarViewController}. */
