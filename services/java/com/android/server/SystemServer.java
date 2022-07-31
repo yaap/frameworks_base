c98692ded047496b360f3170fd3c4efb01767be8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 10:06:14 +0300

    add helpers for system_server extensions

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -3213,6 +3213,8 @@ public final class SystemServer implements Dumpable {
                 reportWtf("Triggering OdsignStatsLogger", e);
             }
             t.traceEnd();
+
+            com.android.server.ext.SystemServerExt.init(mSystemContext, mPackageManagerService);
         }, t);
 
         t.traceBegin("LockSettingsThirdPartyAppsStarted");
