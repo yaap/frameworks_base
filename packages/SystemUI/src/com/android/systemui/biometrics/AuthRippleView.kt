4587b3794c314b54a767e18dd8270052c5735dd6
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-25 14:20:48 +0200

    SystemUI: Allow overlaying auth ripple animation duration
    And make it shorter by 50% by default

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleView.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleView.kt
@@ -56,6 +56,7 @@ class AuthRippleView(context: Context?, attrs: AttributeSet?) : View(context, at
     private val fadeDuration = 83L
     private val retractDuration = 400L
     private var alphaInDuration: Long = 0
+    private var animationDuration: Long = 1533
     private var unlockedRippleInProgress: Boolean = false
     private val dwellShader = DwellRippleShader()
     private val dwellPaint = Paint()
@@ -112,6 +113,10 @@ class AuthRippleView(context: Context?, attrs: AttributeSet?) : View(context, at
 
     fun setAlphaInDuration(duration: Long) {
         alphaInDuration = duration
+    }
+
+    fun setAnimationDuration(duration: Long) {
+        animationDuration = duration
     }
 
     /**
@@ -269,7 +274,7 @@ class AuthRippleView(context: Context?, attrs: AttributeSet?) : View(context, at
 
         val rippleAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
             interpolator = Interpolators.LINEAR_OUT_SLOW_IN
-            duration = AuthRippleController.RIPPLE_ANIMATION_DURATION
+            duration = animationDuration
             addUpdateListener { animator ->
                 val now = animator.currentPlayTime
                 rippleShader.progress = animator.animatedValue as Float
