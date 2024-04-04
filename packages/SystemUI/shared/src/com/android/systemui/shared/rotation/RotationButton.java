9dcda22c14e9ced8916f412a034f5786c7b92d41
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-05 00:33:51 +0300

    SystemUI: Fixup allow toggling floating rotation button

--- packages/SystemUI/shared/src/com/android/systemui/shared/rotation/RotationButton.java
+++ packages/SystemUI/shared/src/com/android/systemui/shared/rotation/RotationButton.java
@@ -36,7 +36,7 @@ public interface RotationButton {
     default boolean isVisible() {
         return false;
     }
-    default void setCanShowRotationButton(boolean canShow) {}
+    default void setCanShowRotationButton(boolean canShow) {} // don't remove me when AOSP does
     default void onTaskbarStateChanged(boolean taskbarVisible, boolean taskbarStashed) {}
     default void updateIcon(int lightIconColor, int darkIconColor) { }
     default void setOnClickListener(View.OnClickListener onClickListener) { }
