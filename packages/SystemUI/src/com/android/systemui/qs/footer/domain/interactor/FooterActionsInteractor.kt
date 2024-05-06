6e40f11b4de610ace508fe2fa8d32c323708d941
Ido Ben-Hur <idoybh2@gmail.com>
2024-05-06 20:57:23 +0300

    SystemUI: Add vibration when long pressing QS footer settings button

--- packages/SystemUI/src/com/android/systemui/qs/footer/domain/interactor/FooterActionsInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/domain/interactor/FooterActionsInteractor.kt
@@ -23,6 +23,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.UserHandle
+import android.os.VibrationEffect
 import android.provider.Settings
 import com.android.internal.jank.InteractionJankMonitor
 import com.android.internal.logging.MetricsLogger
@@ -41,6 +42,7 @@ import com.android.systemui.qs.footer.data.model.UserSwitcherStatusModel
 import com.android.systemui.qs.footer.data.repository.ForegroundServicesRepository
 import com.android.systemui.qs.footer.domain.model.SecurityButtonConfig
 import com.android.systemui.security.data.repository.SecurityRepository
+import com.android.systemui.statusbar.VibratorHelper
 import com.android.systemui.statusbar.policy.DeviceProvisionedController
 import com.android.systemui.user.data.repository.UserSwitcherRepository
 import com.android.systemui.user.domain.interactor.UserSwitcherInteractor
@@ -107,6 +109,7 @@ constructor(
     private val qsSecurityFooterUtils: QSSecurityFooterUtils,
     private val fgsManagerController: FgsManagerController,
     private val userSwitcherInteractor: UserSwitcherInteractor,
+    private val vibrator: VibratorHelper,
     securityRepository: SecurityRepository,
     foregroundServicesRepository: ForegroundServicesRepository,
     userSwitcherRepository: UserSwitcherRepository,
@@ -189,6 +192,7 @@ constructor(
         }
 
         metricsLogger.action(MetricsProto.MetricsEvent.ACTION_QS_EXPANDED_SETTINGS_LAUNCH)
+        if (vibrator != null) vibrator.vibrate(VibrationEffect.EFFECT_HEAVY_CLICK)
         activityStarter.startActivity(
             Intent(Settings.ACTION_RUNNING_SERVICES_SETTING),
             true /* dismissShade */,
