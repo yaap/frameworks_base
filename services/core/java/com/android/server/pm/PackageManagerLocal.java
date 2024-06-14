9f9a4081de5f615dd48e4f50c7e3aab1f68fee1d
Ido Ben-Hur <idoybh2@gmail.com>
2024-06-14 21:12:48 +0300

    Revert "add notification-based UI for the background dexopt job"
    This reverts commit de2b73b13133c2af235fefbcfd193551d15426eb.

--- services/core/java/com/android/server/pm/PackageManagerLocal.java
+++ services/core/java/com/android/server/pm/PackageManagerLocal.java
@@ -16,7 +16,6 @@
 
 package com.android.server.pm;
 
-import android.annotation.ElapsedRealtimeLong;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -203,11 +202,4 @@ public interface PackageManagerLocal {
     }
 
     void showDexoptProgressBootMessage(int percentage, int current, int total);
-
-    void onBgDexoptProgressUpdate(@ElapsedRealtimeLong long start, int percentage, int current, int total);
-
-    void onBgDexoptCompleted(
-            // com.android.server.art.model.DexoptResult
-            @Nullable Object dexOptResult,
-            long durationMs);
 }
