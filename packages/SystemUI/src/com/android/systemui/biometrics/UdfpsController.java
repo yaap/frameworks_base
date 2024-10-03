086775f9b62ff60b984f332bb0d91d60e35883ca
Simão Gomes Viana <devel@superboring.dev>
2024-10-03 16:30:53 +0000

    UdfpsController: improve UDFPS on AOD
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -909,6 +909,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         } else {
             if (view != null) {
                 UdfpsView udfpsView = (UdfpsView) view;
+                cancelAodSendFingerUpAction();
                 if (udfpsView.isDisplayConfigured()) {
                     udfpsView.unconfigureDisplay();
                 }
@@ -1139,6 +1140,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             return;
         }
         if (isOptical()) {
+            onAodInterrupt((int) x, (int) y, major, minor);
             mLatencyTracker.onActionStart(ACTION_UDFPS_ILLUMINATE);
         }
         // Refresh screen timeout and boost process priority if possible.
