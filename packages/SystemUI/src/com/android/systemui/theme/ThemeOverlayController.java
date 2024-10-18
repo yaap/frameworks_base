658e23ab15ea7f97d8c32160c85343e5773b1d83
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-18 20:51:39 +0300

    [SQUASH] base: Revert dark background toggle [1/3]
    Revert "base: ThemeOverlayController: Force enable neutral overlay on day theme"
    
    This reverts commit fafd4e02cffa8e92f32f390af25d94770829a4ba.
    
    Revert "ThemeOverlayController: Don't enable neutral overlay when we use dark bg"
    
    This reverts commit 79a11f08a6e739d51492ca9e8874a11a1a16334a.
    
    Revert "ThemeOverlayController: Don't enable neutral overlay on boot as well"
    
    This reverts commit 45fdbae2679605f01ebe1bcc8108d62388c0243f.

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -45,9 +45,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.om.FabricatedOverlay;
-import android.content.om.IOverlayManager;
 import android.content.om.OverlayIdentifier;
-import android.content.om.OverlayInfo;
 import android.content.pm.UserInfo;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -55,8 +53,6 @@ import android.database.ContentObserver;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Handler;
-import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -127,8 +123,6 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     protected static final String TAG = "ThemeOverlayController";
     private static final boolean DEBUG = true;
 
-    private static final String DARK_OVERLAY_NAME = "com.android.yaap.dark_bg";
-
     private final ThemeOverlayApplier mThemeManager;
     private final UserManager mUserManager;
     private final BroadcastDispatcher mBroadcastDispatcher;
@@ -175,8 +169,6 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     private final UiModeManager mUiModeManager;
     private ColorScheme mDarkColorScheme;
     private ColorScheme mLightColorScheme;
-
-    private IOverlayManager mOverlayManager;
 
     // Defers changing themes until Setup Wizard is done.
     private boolean mDeferredThemeEvaluation;
@@ -454,9 +446,6 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         mUiModeManager = uiModeManager;
         mActivityManager = activityManager;
         dumpManager.registerDumpable(TAG, this);
-
-        mOverlayManager = IOverlayManager.Stub.asInterface(
-                ServiceManager.getService(Context.OVERLAY_SERVICE));
     }
 
     @Override
@@ -792,26 +781,10 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
             }
         }
 
-        boolean nightMode = (mResources.getConfiguration().uiMode
-                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
-        boolean skipNeutral = false;
-        boolean enableNeutral = false;
-        if (mOverlayManager != null) {
-            OverlayInfo info = null;
-            try {
-                info = mOverlayManager.getOverlayInfo(DARK_OVERLAY_NAME, mUserTracker.getUserId());
-            } catch (RemoteException e) {
-                Log.e(TAG, "Failed getting overlay " + DARK_OVERLAY_NAME + " info");
-                e.printStackTrace();
-            }
-            skipNeutral = nightMode && info != null && info.isEnabled();
-            enableNeutral = !nightMode && info != null && info.isEnabled();
-        }
-
         // Compatibility with legacy themes, where full packages were defined, instead of just
         // colors.
         if (!categoryToPackage.containsKey(OVERLAY_CATEGORY_SYSTEM_PALETTE)
-                && mNeutralOverlay != null && (!skipNeutral || enableNeutral)) {
+                && mNeutralOverlay != null) {
             categoryToPackage.put(OVERLAY_CATEGORY_SYSTEM_PALETTE,
                     mNeutralOverlay.getIdentifier());
         }
@@ -853,14 +826,9 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
 
         if (mNeedsOverlayCreation) {
             mNeedsOverlayCreation = false;
-            FabricatedOverlay[] fOverlay = new FabricatedOverlay[skipNeutral ? 2 : 3];
-            int c = 0;
-            fOverlay[c++] = mSecondaryOverlay;
-            if (!skipNeutral || enableNeutral) fOverlay[c++] = mNeutralOverlay;
-            fOverlay[c++] = mDynamicOverlay;
-            mThemeManager.applyCurrentUserOverlays(categoryToPackage, fOverlay,
-                    currentUser, managedProfiles, onCompleteCallback);
-            return;
+            fOverlays = new FabricatedOverlay[]{
+                    mSecondaryOverlay, mNeutralOverlay, mDynamicOverlay
+            };
         }
 
         mThemeManager.applyCurrentUserOverlays(categoryToPackage, fOverlays, currentUser,
