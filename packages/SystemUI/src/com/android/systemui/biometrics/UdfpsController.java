6fee6e5f03c16cc213c33b36729e05b3ab39b237
Austin Delgado <austindelgado@google.com>
2025-03-25 19:01:04 +0000

    Move BP window to Keyguard_dialog
    Bug: 384727394
    Test: UdfpsControllerTest
    Test: UdfpsControllerOverlayTest
    Flag: EXEMPT bugfix
    
    Change-Id: Ifce421be7997eca4323a2e0412abca5753b6b787
    (cherry picked from commit 47fd897138eb2c9ee3a25ca35970d34178226f6a)

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -86,6 +86,7 @@ import com.android.systemui.biometrics.udfps.TouchProcessorResult;
 import com.android.systemui.biometrics.ui.view.UdfpsTouchOverlay;
 import com.android.systemui.biometrics.ui.viewmodel.DefaultUdfpsTouchOverlayViewModel;
 import com.android.systemui.biometrics.ui.viewmodel.DeviceEntryUdfpsTouchOverlayViewModel;
+import com.android.systemui.biometrics.ui.viewmodel.PromptUdfpsTouchOverlayViewModel;
 import com.android.systemui.bouncer.domain.interactor.AlternateBouncerInteractor;
 import com.android.systemui.bouncer.domain.interactor.PrimaryBouncerInteractor;
 import com.android.systemui.dagger.SysUISingleton;
@@ -181,6 +182,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             mDeviceEntryUdfpsTouchOverlayViewModel;
     @NonNull private final Lazy<DefaultUdfpsTouchOverlayViewModel>
             mDefaultUdfpsTouchOverlayViewModel;
+    @NonNull private final Lazy<PromptUdfpsTouchOverlayViewModel>
+            mPromptUdfpsTouchOverlayViewModel;
     @NonNull private final AlternateBouncerInteractor mAlternateBouncerInteractor;
     @NonNull private final UdfpsOverlayInteractor mUdfpsOverlayInteractor;
     @NonNull private final PowerInteractor mPowerInteractor;
@@ -276,18 +279,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             mUdfpsOverlayInteractor.setRequestId(requestId);
             mFgExecutor.execute(() -> UdfpsController.this.showUdfpsOverlay(
                     new UdfpsControllerOverlay(
-                        mContext,
                         mInflater,
                         mWindowManager,
                         mAccessibilityManager,
-                        mStatusBarStateController,
-                        mKeyguardViewManager,
                         mKeyguardUpdateMonitor,
-                        mDialogManager,
-                        mDumpManager,
-                        mConfigurationController,
                         mKeyguardStateController,
-                        mUnlockedScreenOffAnimationController,
                         mUdfpsDisplayMode,
                         requestId,
                         reason,
@@ -296,14 +292,10 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                             requestId,
                             event
                         ),
-                            mActivityTransitionAnimator,
-                        mPrimaryBouncerInteractor,
-                        mAlternateBouncerInteractor,
                         mKeyguardTransitionInteractor,
-                        mSelectedUserInteractor,
                         mDeviceEntryUdfpsTouchOverlayViewModel,
                         mDefaultUdfpsTouchOverlayViewModel,
-                        mShadeInteractor,
+                        mPromptUdfpsTouchOverlayViewModel,
                         mUdfpsOverlayInteractor,
                         mPowerInteractor,
                         mScope
@@ -701,6 +693,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             @NonNull KeyguardTransitionInteractor keyguardTransitionInteractor,
             Lazy<DeviceEntryUdfpsTouchOverlayViewModel> deviceEntryUdfpsTouchOverlayViewModel,
             Lazy<DefaultUdfpsTouchOverlayViewModel> defaultUdfpsTouchOverlayViewModel,
+            Lazy<PromptUdfpsTouchOverlayViewModel> promptUdfpsTouchOverlayViewModel,
             @NonNull UdfpsOverlayInteractor udfpsOverlayInteractor,
             @NonNull PowerInteractor powerInteractor,
             @Application CoroutineScope scope,
@@ -756,6 +749,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mSessionTracker = sessionTracker;
         mDeviceEntryUdfpsTouchOverlayViewModel = deviceEntryUdfpsTouchOverlayViewModel;
         mDefaultUdfpsTouchOverlayViewModel = defaultUdfpsTouchOverlayViewModel;
+        mPromptUdfpsTouchOverlayViewModel = promptUdfpsTouchOverlayViewModel;
 
         mDumpManager.registerDumpable(TAG, this);
 
@@ -831,7 +825,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                     + " isn't running on keyguard. Skip show.");
             return;
         }
-        if (overlay.show(this, mOverlayParams)) {
+        if (overlay.show(mOverlayParams)) {
             Log.d(TAG, "showUdfpsOverlay | adding window reason=" + requestReason);
             mOnFingerDown = false;
             mAttemptedToDismissKeyguard = false;
