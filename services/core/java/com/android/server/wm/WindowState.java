752e99e7d4d4df23d20ec916a3529a50cac088ea
Riddle Hsu <riddlehsu@google.com>
2025-05-19 15:07:16 +0800

    Use consistent animation when forcibly hiding non system overlay
    There are several non system overlay window types. When hiding the
    windows with animation, their specified exit animations may be used.
    
    This avoids ugly jump cut and inconsistent duration when these
    windows are requested to be hidden.
    
    Bug: 408215749
    Flag: EXEMPT bugfix
    Test: Show multiple windows with different overlay types and
          window animations by non system apps. Launch Settings
          and check the windows are fading out in a short time.
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:dd71ab6ba3950978bd28dc38fecec236cf1153ea)
    Merged-In: Ie7bb2663892d608715aa077e2170eae4c03a4e36
    Change-Id: Ie7bb2663892d608715aa077e2170eae4c03a4e36

--- services/core/java/com/android/server/wm/WindowState.java
+++ services/core/java/com/android/server/wm/WindowState.java
@@ -2997,6 +2997,10 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
         return true;
     }
 
+    boolean isForceHiddenNonSystemOverlayWindow() {
+        return mForceHideNonSystemOverlayWindow;
+    }
+
     void setForceHideNonSystemOverlayWindowIfNeeded(boolean forceHide) {
         final int baseType = getBaseType();
         if (mSession.mCanAddInternalSystemWindow
