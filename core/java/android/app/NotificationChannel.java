ab120082d51e877895efdcfeafebd73b0d97bb93
Alisa Hung <alisahung@google.com>
2025-06-13 05:52:18 -0700

    Trim oversized strings in setId and setConversationId
    Flag: EXEMPT bugfix
    Test: android.app.NotificationChannelTest
    Test: cts NotificationChannelTest
    Bug: 419014146
    Change-Id: Id721d3123ee8d38753f550fe57ba0f5d15d743ac
    (cherry picked from commit a9a1b4365b8d6def5b36fed7dd9caf2389fe0a51)

--- core/java/android/app/NotificationChannel.java
+++ core/java/android/app/NotificationChannel.java
@@ -654,7 +654,7 @@ public final class NotificationChannel implements Parcelable {
      * @hide
      */
     public void setId(String id) {
-        mId = id;
+        mId = getTrimmedString(id);
     }
 
     // Modifiable by apps on channel creation.
@@ -909,8 +909,8 @@ public final class NotificationChannel implements Parcelable {
      */
     public void setConversationId(@NonNull String parentChannelId,
             @NonNull String conversationId) {
-        mParentId = parentChannelId;
-        mConversationId = conversationId;
+        mParentId = getTrimmedString(parentChannelId);
+        mConversationId = getTrimmedString(conversationId);
     }
 
     /**
