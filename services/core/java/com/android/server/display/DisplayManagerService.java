262fc601d0aca3da471b001a097ac29c9c060bf3
Ido Ben-Hur <idoybh2@gmail.com>
2025-12-12 19:07:23 +0200

    Revert "base: Add support for LMOFreeform service"
    This reverts commit c0deed983cffd26cff8b92e682d059399b3694e3.

--- services/core/java/com/android/server/display/DisplayManagerService.java
+++ services/core/java/com/android/server/display/DisplayManagerService.java
@@ -208,8 +208,6 @@ import com.android.server.wm.DesktopModeHelper;
 import com.android.server.wm.SurfaceAnimationThread;
 import com.android.server.wm.WindowManagerInternal;
 
-import com.libremobileos.freeform.ILMOFreeformDisplayCallback;
-
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -564,8 +562,6 @@ public final class DisplayManagerService extends SystemService {
     // Whether default display should be included in the display topology. Note that this should
     // only be used for the devices in projected mode.
     private boolean mIncludeDefaultDisplayInTopology;
-
-    private LMOFreeformDisplayAdapter mFreeformDisplayAdapter;
 
     private final BroadcastReceiver mIdleModeReceiver = new BroadcastReceiver() {
         @Override
@@ -2390,7 +2386,6 @@ public final class DisplayManagerService extends SystemService {
             if (shouldRegisterNonEssentialDisplayAdaptersLocked()) {
                 registerOverlayDisplayAdapterLocked();
                 registerWifiDisplayAdapterLocked();
-                registerFreeformDisplayAdapterLocked();
             }
         }
     }
@@ -2409,13 +2404,6 @@ public final class DisplayManagerService extends SystemService {
                     mPersistentDataStore, mFlags);
             registerDisplayAdapterLocked(mWifiDisplayAdapter);
         }
-    }
-
-    private void registerFreeformDisplayAdapterLocked() {
-        mFreeformDisplayAdapter = new LMOFreeformDisplayAdapter(
-                mSyncRoot, mContext, mHandler, mDisplayDeviceRepo, mLogicalDisplayMapper,
-                mUiHandler, mFlags);
-        registerDisplayAdapterLocked(mFreeformDisplayAdapter);
     }
 
     private boolean shouldRegisterNonEssentialDisplayAdaptersLocked() {
@@ -6441,23 +6429,6 @@ public final class DisplayManagerService extends SystemService {
             if (userId == mCurrentUserId) {
                 scheduleTopologiesReload(mCurrentUserId, /*isUserSwitching=*/ false);
             }
-        }
-
-        public void createFreeformLocked(String name, ILMOFreeformDisplayCallback callback,
-                int width, int height, int densityDpi, boolean secure, boolean ownContentOnly,
-                boolean shouldShowSystemDecorations, Surface surface, float refreshRate,
-                long presentationDeadlineNanos) {
-            mFreeformDisplayAdapter.createFreeformLocked(name, callback, width, height, densityDpi,
-                    secure, ownContentOnly, shouldShowSystemDecorations, surface, refreshRate,
-                    presentationDeadlineNanos);
-        }
-
-        public void resizeFreeform(IBinder appToken, int width, int height, int densityDpi) {
-            mFreeformDisplayAdapter.resizeFreeform(appToken, width, height, densityDpi);
-        }
-
-        public void releaseFreeform(IBinder appToken) {
-            mFreeformDisplayAdapter.releaseFreeform(appToken);
         }
     }
 
